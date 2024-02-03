0e38f55a55c7dd849ade8f11c249d7537386937e
Luuuuna <vvaskaa@list.ru>
2024-02-03 14:14:06 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const url = "https://content.guardianapis.com/search";
+    const apiKey = "4f7b9cce-c007-4eee-9176-e705b8a4f561";
+
+    fetch(`${url}?q=${inputValue}&api-key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.response.results);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+
+  };
+
+
+  return (
+    <div className="App">
+     <div className="App__header">
+      <h1>
+        Получите самые свежие новости от газеты <br/>
+        <span>The Guardian!</span>
+      </h1>
+      <h2>Укажите ключевое слово.</h2>
+      <form onSubmit={handleFormSubmit}>
+        
+        <input 
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          className="newsinput"
+          placeholder="введите интересующую тему"
+        /> <br />
+        <button type="submit" className="button">Submit</button>
+      </form>
+      </div>
+      <ol className="inform">
+        {news.map((item) => (
+          <li key={item.id}>
+             <p>{item.sectionId}</p>
+            <a href={item.webUrl}>{item.webTitle}</a>
+              <p>{item.webUrl}</p> 
+          </li>
+        ))}
+      </ol>
+
+    </div>
+  );
+};
+
+export default App;
